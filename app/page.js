bea7f8a8c2501affee4f9e4dbb818286aa51ae79
Shriram <shriram1am@gmail.com>
2026-03-27 16:44:23 +0530

    Now all features are add with respect to deployment

--- app/page.js
+++ app/page.js
@@ -1,12 +1,16 @@
 import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
 import Footer from '@/components/Footer';
+import About from '@/components/About';
+import ProgramsSection from '@/components/ProgramsSection';
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <Navbar />
       <Hero />
+      <About/>
+      <ProgramsSection/>
       <Footer/>
     </main>
 
